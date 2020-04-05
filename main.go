6bfc8344c2c703df45b9b44c1519d2028e95ec68
Hannah Steingass <steingassh@icloud.com>
2020-04-05 16:01:24 -0400

    added an option for a blur effect on the output image

--- main.go
+++ main.go
@@ -33,6 +33,7 @@ var (
 	Nth        int
 	Repeat     int
 	Filter     int
+	Blur       float64
 	Rotation   float64
 	V, VV      bool
 )
@@ -81,6 +82,7 @@ func init() {
 	flag.IntVar(&Nth, "nth", 1, "save every Nth frame (put \"%d\" in path)")
 	flag.IntVar(&Repeat, "rep", 0, "add N extra shapes per iteration with reduced search")
 	flag.IntVar(&Filter, "f", 0, "0=no filter 1=gray scale 2=sepia 3=negative")
+	flag.Float64Var(&Blur, "blur", 0, "produces a blurred version of the image, must be a positive value")
 	flag.Float64Var(&Rotation, "rot", 0, "degree of rotation for the output image")
 	flag.BoolVar(&V, "v", false, "verbose")
 	flag.BoolVar(&VV, "vv", false, "very verbose")
